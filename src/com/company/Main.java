c68ba6e4d68f3daf42b4257a608922de4ef7ee69
DESKTOP-BO8H4UB\admin <d.akhmetzyanov.stc@innopolis.ru>
2017-04-05 16:13:29 +0300

    Show data in different library object library2 after deserialization.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,30 +12,44 @@ public class Main {
     public static void main(String[] args) {
         Library library = new Library();
 
-        for (Book book:DataManager.deserializeFromFile()
-                ) {
-            library.buyBook(book.getTitle(), book.getAuthor(), book.getISBN(), 1, book.getYear());
-        }
+
 
         Reader reader = new Reader(97094325, "John", "Ivanovich", "Conor");
         Reader reade2 = new Reader(97092389, "Sara", "Petrovich", "Conor");
 
-//        library.buyBook("Intro to Java", "Petrov", "124587ps", 5, 1987);
-//        library.buyBook("Intro to C", "Stepanov", "234162ps", 2, 1999);
+        library.buyBook("Intro to Java", "Petrov", "124587ps", 5, 1987);
+        library.buyBook("Intro to C", "Stepanov", "234162ps", 2, 1999);
 
         library.takeBook("John", "Ivanovich", "Conor", 97094325, "Intro to Java");
         library.takeBook("Sara", "Petrovich", "Conor", 97092389, "Intro to C");
 
         library.returnBook("John", "Ivanovich", "Conor", 97094325, "Intro to Java");
 
-        library.showAllData();
 
 
-//        DataManager.serializeToFile(library.getCatalog());
-
-
+        DataManager.serializeToFile(library.getCatalog());
 
 
 
+
+
+        
+        //=========================== Show data ======================================
+
+
+
+        Library library2 = new Library();
+
+        for (Book book:DataManager.deserializeFromFile()
+                ) {
+            library2.buyBook(book.getTitle(), book.getAuthor(), book.getISBN(), 1, book.getYear());
+        }
+
+        library2.takeBook("John", "Ivanovich", "Conor", 97094325, "Intro to Java");
+        library2.takeBook("Sara", "Petrovich", "Conor", 97092389, "Intro to C");
+
+        library2.returnBook("John", "Ivanovich", "Conor", 97094325, "Intro to Java");
+
+        library2.showAllData();
     }
 }
